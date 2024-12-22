fa6a1c156430008c6b6076dca68d87ab3beb905b
Clatson <124655279+clatsonin@users.noreply.github.com>
2024-12-22 19:40:09 +0530

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,17 @@
+from urllib import request
+
+from flask import Flask
+app = Flask(__name__)
+from twilio.twiml.messaging_response import MessagingResponse
+@app.route('/sms', methods=['POST'])
+def sms():
+    print(request.form)
+    resp = MessagingResponse()
+    resp.message("Hello boy!")
+    return str(resp)
+@app.route('/')
+def index():
+    return "Hello all"
+
+if __name__ == '__main__':
+    app.run(debug=True)
