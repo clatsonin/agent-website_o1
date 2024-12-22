39a6202ad61ef28773a799121853833d1b9956d9
Clatson <124655279+clatsonin@users.noreply.github.com>
2024-12-22 19:52:38 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+from twilio.twiml.messaging_response import MessagingResponse
 
+@app.route('/sms', methods=['POST'])
+def sms():
+    print(request.form)
+    resp = MessagingResponse()
+    resp.message("Hello boy!")
+    return str(resp)
+    
 @app.route('/')
 def index():
     return "Hello all"
